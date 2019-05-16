34e101e4665083b59f52efb42efb4aeb75ee5c69
Rob <orucrob@gmail.com>
2019-05-16 18:09:44 +0200

    lambda authorizer

--- onconnect/app.js
+++ onconnect/app.js
@@ -3,6 +3,7 @@ AWS.config.update({ region: process.env.AWS_REGION })
 var DDB = new AWS.DynamoDB({ apiVersion: '2012-10-08' })
 
 exports.handler = function(event, context, callback) {
+    console.log('Received $onconnect event:', JSON.stringify(event, null, 2))
     var putParams = {
         TableName: process.env.TABLE_NAME,
         Item: {
